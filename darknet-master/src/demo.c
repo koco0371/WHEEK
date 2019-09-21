4f4864b7ce782a862b44af96f0b8e2788f00e109
sonyyhh <40329796+sonyyhh@users.noreply.github.com>
2019-09-21 20:39:49 +0900

    190921
    - 우리가 만든 코드 삽입

--- darknet-master/src/demo.c
+++ darknet-master/src/demo.c
@@ -7,12 +7,17 @@
 #include "box.h"
 #include "image.h"
 #include "demo.h"
+
+#include <X11/extensions/XTest.h> //
+#include <unistd.h> //
+
 #ifdef WIN32
 #include <time.h>
 #include "gettimeofday.h"
 #else
 #include <sys/time.h>
 #endif
+
 
 
 #ifdef OPENCV
@@ -27,7 +32,7 @@ static int nboxes = 0;
 static detection *dets = NULL;
 
 static network net;
-static image in_s ;
+static image in_s;
 static image det_s;
 
 static cap_cv *cap;
@@ -52,14 +57,198 @@ mat_cv* show_img;
 static volatile int flag_exit;
 static int letter_box = 0;
 
+#define NOTHING -1
+#define PALM 0
+#define FIST 1
+
+#define SCALE 10 //bbox 변화를 마우스 포인터 이동에 적용하기 위한 배율 -> 임시: 10
+
+int cur_state = NOTHING;
+int prev_state = NOTHING;
+int prev_x = 0;
+int prev_y = 0;
+int cur_x = 0;
+int cur_y = 0;
+
+//웹캠에서 인식된 손 bbox의 x변화 구함
+int get_x_distance()
+{
+    return cur_x - prev_x;
+}
+
+//웹캠에서 인식된 손 bbox의 y변화 구함
+int get_y_distance()
+{
+    return cur_y - prev_y;
+}
+
+void left_down(int x, int y)
+{
+    Display *dpy = NULL;
+    XEvent event;
+    dpy = XOpenDisplay(NULL);
+
+
+    XQueryPointer(dpy, RootWindow(dpy, 0), &event.xbutton.root,
+        &event.xbutton.window, &event.xbutton.x_root,
+        &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y,
+        &event.xbutton.state);
+
+    XTestFakeButtonEvent(dpy, 1, True, CurrentTime);
+    XSync(dpy, 0);
+    sleep(0.5);
+    XCloseDisplay(dpy);
+}
+
+
+
+void left_up(int x, int y)
+{
+    Display *dpy = NULL;
+    XEvent event;
+    dpy = XOpenDisplay(NULL);
+
+
+    XQueryPointer(dpy, RootWindow(dpy, 0), &event.xbutton.root,
+        &event.xbutton.window, &event.xbutton.x_root,
+        &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y,
+        &event.xbutton.state);
+
+    XTestFakeButtonEvent(dpy, 1, False, CurrentTime);
+    XSync(dpy, 0);
+    sleep(0.5);
+    XCloseDisplay(dpy);
+
+}
+
+void left_click(int x, int y)
+{
+    if (prev_state == PALM && cur_state == FIST)
+    {
+        left_down(x, y);
+    }
+    else if (prev_state == FIST && cur_state == PALM)
+    {
+        left_up(x, y);
+    }
+    prev_state = cur_state;
+    prev_x = cur_x;
+    prev_y = cur_y;
+}
+
+void drag_fist(int x, int y)
+{
+    Display *dpy = NULL;
+    XEvent event;
+    dpy = XOpenDisplay(NULL);
+
+    /* Get the current pointer position */
+    XQueryPointer(dpy, RootWindow(dpy, 0), &event.xbutton.root,
+        &event.xbutton.window, &event.xbutton.x_root,
+        &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y,
+        &event.xbutton.state);
+
+    /* Fake the pointer movement to new relative position */
+    /* 마우스 포인터의 상대위치 변화: get_x_distance(), get_y_distance(). 배율 미정 */
+    XTestFakeMotionEvent(dpy, 0, event.xbutton.x + get_x_distance()*SCALE, event.xbutton.y + get_y_distance()*SCALE, CurrentTime);
+    XSync(dpy, 0);
+    sleep(0.5);
+    XCloseDisplay(dpy);
+
+}
+
+void move_pointer(int x, int y)
+{
+    Display *dpy = NULL;
+    XEvent event;
+    dpy = XOpenDisplay(NULL);
+
+    /* Get the current pointer position */
+    XQueryPointer(dpy, RootWindow(dpy, 0), &event.xbutton.root,
+        &event.xbutton.window, &event.xbutton.x_root,
+        &event.xbutton.y_root, &event.xbutton.x, &event.xbutton.y,
+        &event.xbutton.state);
+
+    /* Fake the pointer movement to new relative position */
+    /* 마우스 포인터의 상대위치 변화: get_x_distance(), get_y_distance(). 배율 미정 */
+    XTestFakeMotionEvent(dpy, 0, event.xbutton.x + get_x_distance()*SCALE, event.xbutton.y + get_y_distance()*SCALE, CurrentTime);
+    XSync(dpy, 0);
+    sleep(0.5);
+    XCloseDisplay(dpy);
+
+}
+
+//드래그하는 정도(배율)은 테스트하면서 결정
+void drag(int x, int y)
+{
+    if (prev_state == FIST && cur_state == FIST)
+    {
+        drag_fist(x, y);
+    }
+    else if (prev_state == PALM && cur_state == PALM)
+    {
+        move_pointer(x, y);
+    }
+    prev_state = cur_state;
+    prev_x = cur_x;
+    prev_y = cur_y;
+}
+void click_release(int x, int y) {
+    if (prev_state == FIST) {
+        left_up(x, y);
+    }
+}
+void detect_hand(int x, int y) {
+    prev_state = cur_state;
+}
+
+
+void control_display(detection* sorted_dets, float thresh, char** names, int classes, int num) {
+    int i, j, num;	//num-> scene 내의 bounding box 개수
+    int class_id;
+    for (i = 0; i < num; ++i) {
+        class_id = -1;
+        for (j = 0; j < classes; ++j) {
+            int show = strncmp(names[j], "dont_show", 9);
+            if (dets[i].prob[j] > thresh && show) {
+                if (class_id < 0) {
+                    class_id = j;
+                }
+            }
+        }
+    }
+    cur_state = class_id;
+    //det 박스의 중점x,y를 구한다
+    cur_x = sorted_dets[0].bbox.x;
+    cur_y = sorted_dets[0].bbox.y;
+
+    //det 가 어떤 클래스에 가장 부합한지 찾는다
+    if (cur_state == NOTHING) {
+        click_release(cur_x, cur_y);
+        prev_state = NOTHING; //다음에 다시 시작할땐 처음 상태부터 시작
+        return;
+    }
+    else if (cur_state != prev_state) {
+        if (prev_state == NOTHING && cur_state == PALM) {
+            detect_hand(cur_x, cur_y);
+        }
+        else {
+            left_click(cur_x, cur_y);
+        }
+    }
+    else if (cur_state == prev_state) {
+        drag(cur_x, cur_y);
+    }
+}
+
 void *fetch_in_thread(void *ptr)
 {
     int dont_close_stream = 0;    // set 1 if your IP-camera periodically turns off and turns on video-stream
-    if(letter_box)
+    if (letter_box)
         in_s = get_image_from_stream_letterbox(cap, net.w, net.h, net.c, &in_img, dont_close_stream);
     else
         in_s = get_image_from_stream_resize(cap, net.w, net.h, net.c, &in_img, dont_close_stream);
-    if(!in_s.data){
+    if (!in_s.data) {
         printf("Stream closed.\n");
         flag_exit = 1;
         //exit(EXIT_FAILURE);
@@ -72,11 +261,11 @@ void *fetch_in_thread(void *ptr)
 
 void *detect_in_thread(void *ptr)
 {
-    layer l = net.layers[net.n-1];
+    layer l = net.layers[net.n - 1];
     float *X = det_s.data;
     float *prediction = network_predict(net, X);
 
-    memcpy(predictions[demo_index], prediction, l.outputs*sizeof(float));
+    memcpy(predictions[demo_index], prediction, l.outputs * sizeof(float));
     mean_arrays(predictions, NFRAMES, l.outputs, avg);
     l.output = avg;
 
@@ -106,6 +295,7 @@ double get_wall_time()
 void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int cam_index, const char *filename, char **names, int classes,
     int frame_skip, char *prefix, char *out_filename, int mjpeg_port, int json_port, int dont_show, int ext_output, int letter_box_in)
 {
+    FILE* temptestout = fopen("temptestout.txt", "w");
     letter_box = letter_box_in;
     in_img = det_img = show_img = NULL;
     //skip = frame_skip;
@@ -119,17 +309,18 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
     demo_json_port = json_port;
     printf("Demo\n");
     net = parse_network_cfg_custom(cfgfile, 1, 1);    // set batch=1
-    if(weightfile){
+    if (weightfile) {
         load_weights(&net, weightfile);
     }
     fuse_conv_batchnorm(net);
     calculate_binary_weights(net);
     srand(2222222);
 
-    if(filename){
+    if (filename) {
         printf("video file: %s\n", filename);
         cap = get_capture_video_stream(filename);
-    }else{
+    }
+    else {
         printf("Webcam index: %d\n", cam_index);
         cap = get_capture_webcam(cam_index);
     }
@@ -141,12 +332,12 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
         error("Couldn't connect to webcam.\n");
     }
 
-    layer l = net.layers[net.n-1];
+    layer l = net.layers[net.n - 1];
     int j;
 
-    avg = (float *) calloc(l.outputs, sizeof(float));
-    for(j = 0; j < NFRAMES; ++j) predictions[j] = (float *) calloc(l.outputs, sizeof(float));
-    for(j = 0; j < NFRAMES; ++j) images[j] = make_image(1,1,3);
+    avg = (float *)calloc(l.outputs, sizeof(float));
+    for (j = 0; j < NFRAMES; ++j) predictions[j] = (float *)calloc(l.outputs, sizeof(float));
+    for (j = 0; j < NFRAMES; ++j) images[j] = make_image(1, 1, 3);
 
     if (l.classes != demo_classes) {
         printf("Parameters don't match: in cfg-file classes=%d, in data-file classes=%d \n", l.classes, demo_classes);
@@ -177,9 +368,9 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
     }
 
     int count = 0;
-    if(!prefix && !dont_show){
+    if (!prefix && !dont_show) {
         int full_screen = 0;
-        create_window_cv("Demo", full_screen, 1352, 1013);
+        create_window_cv("Demo", full_screen, 480, 320/*1352, 1013*/);
     }
 
 
@@ -202,11 +393,11 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
 
     double before = get_wall_time();
 
-    while(1){
+    while (1) {
         ++count;
         {
-            if(pthread_create(&fetch_thread, 0, fetch_in_thread, 0)) error("Thread creation failed");
-            if(pthread_create(&detect_thread, 0, detect_in_thread, 0)) error("Thread creation failed");
+            if (pthread_create(&fetch_thread, 0, fetch_in_thread, 0)) error("Thread creation failed");
+            if (pthread_create(&detect_thread, 0, detect_in_thread, 0)) error("Thread creation failed");
 
             float nms = .45;    // 0.4F
             int local_nboxes = nboxes;
@@ -214,6 +405,33 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
 
             //if (nms) do_nms_obj(local_dets, local_nboxes, l.classes, nms);    // bad results
             if (nms) do_nms_sort(local_dets, local_nboxes, l.classes, nms);
+            control_display(local_dets, demo_thresh, demo_names, demo_classes, local_nboxes);
+
+            //print class!!
+            int i;
+            for (i = 0; i < local_nboxes; ++i) {
+                int class_id = -1;
+                float prob = 0;
+
+                for (j = 0; j < l.classes; ++j) {
+                    if (local_dets[i].prob[j] > thresh && local_dets[i].prob[j] > prob) {
+                        if (class_id < 0) {
+                            class_id = j;
+                            fprintf(temptestout, "class name : %d\n", class_id);
+                            fprintf(temptestout, "prob : %f\n", local_dets[i].prob[class_id]);
+                            fprintf(temptestout, "%2.4f %2.4f %2.4f %2.4f\n", local_dets[i].bbox.x, local_dets[i].bbox.y/*,local_dets[i].bbox.w, local_dets[i].bbox.h*/);
+                        }
+                        else {
+                            fprintf(temptestout, "+\n");
+                            fprintf(temptestout, "class name : %d\n", j);
+                            fprintf(temptestout, "prob : %f\n", local_dets[i].prob[j]);
+                            fprintf(temptestout, "%2.4f %2.4f %2.4f %2.4f\n", local_dets[i].bbox.x, local_dets[i].bbox.y/*,local_dets[i].bbox.w, local_dets[i].bbox.h*/);
+                        }
+                    }
+                }
+            }
+            if (local_nboxes != 0) fprintf(temptestout, "---------------\n");
+
 
             //printf("\033[2J");
             //printf("\033[1;1H");
@@ -231,7 +449,7 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
 
             printf("\nFPS:%.1f\n", fps);
 
-            if(!prefix){
+            if (!prefix) {
                 if (!dont_show) {
                     show_image_mat(show_img, "Demo");
                     int c = wait_key_cv(1);
@@ -246,10 +464,11 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
                         flag_exit = 1;
                     }
                 }
-            }else{
+            }
+            else {
                 char buff[256];
                 sprintf(buff, "%s_%08d.jpg", prefix, count);
-                if(show_img) save_cv_jpg(show_img, buff);
+                if (show_img) save_cv_jpg(show_img, buff);
             }
 
             // if you run it with param -mjpeg_port 8090  then open URL in your web-browser: http://localhost:8090
@@ -273,14 +492,14 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
 
             if (flag_exit == 1) break;
 
-            if(delay == 0){
+            if (delay == 0) {
                 show_img = det_img;
             }
             det_img = in_img;
             det_s = in_s;
         }
         --delay;
-        if(delay < 0){
+        if (delay < 0) {
             delay = frame_skip;
 
             //double after = get_wall_time();
